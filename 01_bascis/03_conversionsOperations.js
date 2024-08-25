d852e6510067e6ba51cacb75e5d7e37abbffd4be
Aabdulraheem <116513271+Aabdulraheem@users.noreply.github.com>
2024-08-25 06:47:13 +0000

    converasionsOperations.js

---
+++ 01_bascis/03_conversionsOperations.js
@@ -0,0 +1,29 @@
+let score = "ABDUL"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  
+
+// if you convert values in number 
+// "33" is converted => 33
+// "33abc"  => Nan 
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+
+//""=> false
+//"abdul "=> flase
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
