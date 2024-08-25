560ef01b26f70207b142f7773fcbf0b8c7bbae9d
Aabdulraheem <116513271+Aabdulraheem@users.noreply.github.com>
2024-08-25 05:14:58 +0000

    datatypes

---
+++ 01_bascis/01_variables.js
@@ -0,0 +1,30 @@
+const accountId = 13434
+let accountEmail = "abdulraheem786@gmail.com"
+var accountPassword = "134223"
+accountCity = "vijayawada"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEamil = "abdula@gmail.com"
+accountPassword ="jfjvcjnvs"
+accountCity = "banglore"
+
+
+console.log("accountId");
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// variables
+let x = null;
+let name ="abdul";
+const found = false;
+console.log(name , found , x);
+
+// numbers 
+
+let price = 6;
+let amount = 88.8;
+
+console.log(price, amount);
---
+++ 01_bascis/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // trat all js code as newer version 
+
+// alert (3+3)  // we are using nodenjs ,not browser
+
+console.log(3+3);
+
+console.log("abdul");   // code rediablity 
+
+let name = "abdulraheem"  
+let age = 18             
+let isLoggedin = false    
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// booleam => truw/false
+// mull => standalone value
+// undefines => umndefined values
+// symbols <=> unique
+// object
+
+console.log(typeof "null"); // object
+console.log(typeof "undefind"); // undefined
